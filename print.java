72fd572eb9dd6fd82d3ace9264721f86563bf8bb
unzila <s.49110@iqra.edu.pk>
2022-08-02 09:01:14 -0700

    file updated

--- print.java
+++ print.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
+// Your First Program
+
+class HelloWorld {
     public static void main(String[] args) {
-        // Write your code here
+        System.out.println("Hello, World!"); 
     }
 }
